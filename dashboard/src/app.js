d327e6717325025de07b6184804aa60230c423ef
Ismael Balafrej <ismael.balafrej@usherbrooke.ca>
2018-03-17 15:19:44 -0400

    Renamed subkinect to kinect

--- dashboard/src/app.js
+++ dashboard/src/app.js
@@ -3,6 +3,6 @@ import ROSLIB from 'roslib';
 import $ from 'cash-dom';
 import route from './route';
 import gamestate from './subscribers/gamestate';
-import kinect from './subscribers/subkinect';
+import kinect from './subscribers/kinect';
 
 route(window.location.pathname, true);
