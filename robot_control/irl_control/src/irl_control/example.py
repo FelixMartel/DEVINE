5b13f17df4543a2ee922fbd9ca5c9d6d8bc6febe
Jordan <jordanprincet@gmail.com>
2018-04-04 01:27:01 -0400

    point with 2 arms and head

--- robot_control/irl_control/src/irl_control/example.py
+++ robot_control/irl_control/src/irl_control/example.py
@@ -5,53 +5,120 @@ import rospy
 
 from trajectory_client import TrajectoryClient
 from gripper import Gripper
+import ik
+from marker import Markers
+import tf
 
-def main():
+def main(args):
     """Example to control head, arms and gripper with command line
     """
-    arg_fmt = argparse.RawDescriptionHelpFormatter
-    parser = argparse.ArgumentParser(formatter_class=arg_fmt, description=main.__doc__)
-    required = parser.add_argument_group('required arguments')
-    required.add_argument('-r', '--robot', required=True, choices=['jn0'], help='Which robot?')
-    required.add_argument('-c', '--controller', required=True, choices=['head_controller', 'left_arm_controller', 'right_arm_controller'], help='Which controller?')
-    required.add_argument('-p', '--positions', required=True, help='What goal positions?')
-    required.add_argument('-t', '--time', required=True, help='How much seconds?')
-
-    args = parser.parse_args(rospy.myargv()[1:])
+    # Parse arguments
     robot = args.robot
     controller = args.controller
-    positions = [float(i) for i in args.positions.replace('[', '').replace(']', '').split(',')]
+    arm = None
+    if controller.split('_arm')[0]:
+        arm = controller.split('_')[0]
+
+    joints = args.joints
+    if joints:
+        joints = [float(i) for i in args.joints.replace('[', '').replace(']', '').split(',')]
+    point = args.point
+    if point:
+        point = [float(i) for i in args.point.replace('[', '').replace(']', '').split(',')]
     time = float(args.time)
 
+    # Start ROS node
     print('Init node...')
     node_name = 'irl_control' + '_' + controller
     rospy.init_node(node_name)
     print('Running node \'' + node_name + '\'...')
 
+    # Init TrajectoryClient
     try:
         traj = TrajectoryClient(robot, controller)
+        traj_head = TrajectoryClient(robot, 'head_controller')
     except RuntimeError as err:
         rospy.logerr(err)
         rospy.signal_shutdown(err)
 
-    gripper_left = Gripper(robot, 'left')
+    # Init Gripper
+    gripper = Gripper(robot, arm)
 
-    traj.add_point(positions, time)
+    # Calculate position
+    if point and arm:
+        # Get tf information
+        tf_listener = tf.TransformListener()
+
+        trans_arm = None
+        i = 0
+        while not trans_arm:
+            try:
+                (trans_arm, rot) = tf_listener.lookupTransform('/' + arm[0].upper() + '_shoulder_fixed_link', '/obj', rospy.Time(0))
+                (trans_neck, rot_neck) = tf_listener.lookupTransform('/neck_pan_link', '/obj', rospy.Time(0))
+            except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException) as err:
+                rospy.sleep(0.1)
+                i = i + 1
+                if i == 10:
+                    rospy.logerr(err)
+                    rospy.signal_shutdown(err)
+                else:
+                    continue
+        print('Translation from shoulder_fixed_link to obj:', trans_arm)
+        print('Translation from neck_pan_link to obj:', trans_neck)
+
+        # Calculate inverse kinematic
+        joints_position = ik.arm_pan_tilt(arm, trans_arm[0], trans_arm[1], trans_arm[2])
+        print('Arm Joint Position:', joints_position)
+
+        neck_joints_position = ik.neck_pan_tilt(trans_neck[0], trans_neck[1], trans_neck[2])
+        print('Neck Joint Position:', neck_joints_position)
+
+        # Show markers TODO TEMP
+        markers = Markers([[point[0], point[1], point[2]]])
+        for i in range(0, 100):
+            markers.publish()
+            rospy.sleep(0.01)
+    else:
+        joints_position = joints
+
+    # Accomplish trajectory
+    traj.add_point(joints_position, time)
+    traj_head.add_point(neck_joints_position, time)
+
     traj.start()
+    traj_head.start()
+
     traj.wait(time)
+    traj_head.wait(time)
 
     for i in range(3):
-        gripper_left.open(0.3)
+        gripper.open(0.3)
         rospy.sleep(0.5)
-        gripper_left.open(0.1)
+        gripper.open(0.1)
         rospy.sleep(0.5)
 
     traj.clear()
+
     traj.add_point([0, 0, 0, 0], time)
+    traj_head.add_point([0, 0], time)
+
     traj.start()
+    traj_head.start()
+
     traj.wait(time)
+    traj_head.wait(time)
 
     print('Completed')
 
 if __name__ == '__main__':
-    main()
+    arg_fmt = argparse.RawDescriptionHelpFormatter
+    parser = argparse.ArgumentParser(formatter_class=arg_fmt, description=main.__doc__)
+    required = parser.add_argument_group('required arguments')
+    required.add_argument('-r', '--robot', required=True, choices=['jn0'], help='Which robot?')
+    required.add_argument('-c', '--controller', required=True, choices=['head_controller', 'left_arm_controller', 'right_arm_controller'], help='Which controller?')
+    required.add_argument('-j', '--joints', required=False, help='What joints positions?')
+    required.add_argument('-p', '--point', required=False, help='What 3D point to point?')
+    required.add_argument('-t', '--time', required=True, help='How much seconds?')
+    args = parser.parse_args(rospy.myargv()[1:])
+
+    main(args)
---
+++ robot_control/irl_control/src/irl_control/marker.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+import math
+import rospy
+from visualization_msgs.msg import Marker
+from visualization_msgs.msg import MarkerArray
+import tf
+
+class Markers(object):
+    def __init__(self, markers_position=None):
+        topic = 'visualization_marker_array'
+        self._pub = rospy.Publisher(topic, MarkerArray, queue_size=10)
+        if markers_position:
+            self.set_marker_array(markers_position)
+
+    def publish(self):
+        self._pub.publish(self.marker_array)
+
+    def set_marker_array(self, markers_position):
+        self.marker_array = MarkerArray()
+        marker_id = 0
+        for marker_position in markers_position:
+            marker = Marker()
+            marker.header.frame_id = "/base_link"
+            marker.type = marker.SPHERE
+            marker.action = marker.ADD
+
+            marker.scale.x = 0.1
+            marker.scale.y = 0.1
+            marker.scale.z = 0.1
+
+            marker.color.a = 1.0
+            marker.color.r = 1.0
+            marker.color.g = 1.0
+            marker.color.b = 0.0
+
+            marker.pose.position.x = marker_position[0]
+            marker.pose.position.y = marker_position[1]
+            marker.pose.position.z = marker_position[2]
+
+            marker.id = marker_id
+            marker_id += 1
+
+            self.marker_array.markers.append(marker)
+
+def main():
+    markers_position = [[1, 0.1, 0.5]]
+
+    rospy.init_node('makers_tf')
+    print('Running node \'' + 'markers_tf' + '\' with: ' + str(markers_position))
+
+    markers = Markers(markers_position)
+    tf_broadcaster = tf.TransformBroadcaster()
+    tf_listener = tf.TransformListener()
+
+    rate = rospy.Rate(10.0)
+
+    while not rospy.is_shutdown():
+        markers.publish()
+        tf_broadcaster.sendTransform(markers_position[0], (0.0, 0.0, 0.0, 1.0), rospy.Time.now(), "obj", "base_link")
+        try:
+            (trans1, rot1) = tf_listener.lookupTransform('/R_frame_tool_link', '/obj', rospy.Time(0))
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            continue
+        print('Orientation from top:', math.floor(math.sin(trans1[1]/trans1[0]) * 180 / math.pi))
+        print('Orientation from side:', math.floor(math.sin(trans1[2]/trans1[0]) * 180 / math.pi))
+        rate.sleep()
+
+if __name__ == '__main__':
+    main()
