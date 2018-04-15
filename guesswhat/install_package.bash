c62f2db1cf835b30c180d5f833b2a3e0f6888eea
Felix Martel-Denis <lixtelnis@gmail.com>
2018-04-14 20:06:23 -0400

    Fetch weights from static repo

--- guesswhat/install_package.bash
+++ guesswhat/install_package.bash
@@ -3,5 +3,8 @@
 CURDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/devine_guesswhat"
 echo "export ROS_PACKAGE_PATH=$CURDIR:\$ROS_PACKAGE_PATH" >> ~/.bashrc
 
+wget https://github.com/projetdevine/static/releases/download/v0.0.1/weights.zip
+unzip weights.zip -d devine_guesswhat/data
+
 # Reload env
 exec bash
