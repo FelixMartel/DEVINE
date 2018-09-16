e737c30fbf7ca04e37635dfde8c9fffd109b8300
Ismael Balafrej <ismael.balafrej@usherbrooke.ca>
2018-09-15 23:16:35 -0400

    Zone detection prototype advancements

--- image_processing/devine_image_processing/src/zone_detection.py
+++ image_processing/devine_image_processing/src/zone_detection.py
@@ -1,92 +1,39 @@
+#!/usr/bin/env python2
+'''Zone detection using bright colored squares'''
+
+import rospy
+from std_msgs.msg import String
+
+from ros_image_processor import ImageProcessor, ROSImageProcessingWrapper
+
 import cv2
 import numpy as np
 from enum import Enum
 
-# The idea: 
-# The player zone will be defined by the body_tracking lib
-# Where a player close enough from the robot will be selected
-# While Idle state should just try and look for humans
-# The object area should be pre-found on boot
-# The object area will be recognized by a set of colors
-# The colors will come from post-its/papers
-# The object area should be setable via the dashboard in case of errors
-# The object area will default at (0, 0) motor position
+#import time
 
-# Tracked colors
-class TrackingColor(Enum):
-    FUCHSIA = 1
-    #GREEN = 2
+IMAGE_TOPIC = '/camera/rgb/image_color/compressed' #'/devine/image/zone_detection'
+ZONE_DETECTION_TOPIC = '/zone_detection'
 
-#Hue range of tracked colors
-TRACKED_COLORS_HUE = {
-   # TrackingColor.GREEN: [25, 85],
-    TrackingColor.FUCHSIA: [150, 180]
-}
-
-# Todo: We could use more corners
-#UPPER_LEFT = [TrackingColor.FUCHSIA, TrackingColor.GREEN]
-#BOTTOM_RIGHT = [TrackingColor.GREEN, TrackingColor.FUCHSIA]
-
-video_capture = cv2.VideoCapture(0)
-
-while True:
-    _, frame = video_capture.read()  # Read the next image from the camera
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-    # Blur
-    hsv = cv2.blur(hsv, (3, 3), (-1, -1))
-
-    # Filter out low saturation pixels
-    mask = cv2.inRange(hsv, (0, 40, 0), (255, 255, 255))
-    hsv = cv2.bitwise_and(frame, frame, mask=mask)
+class ZoneDetection(ImageProcessor):
+    '''Zone detection'''
     
-    hue = hsv[:, :, 0] # Convert to single channel hue representation
-
-    cv2.imshow("Hue representation", hue)
-
-    # Noise reduction equivalent to erosion + dilatation filters
-    hue = cv2.morphologyEx(hue, cv2.MORPH_OPEN, None, iterations=2)
-
-    hsv[:, :, 0] = hue
-    cv2.imshow("Filtered HSV", hsv)
-
-    found_colors = {}
-
-    for tracked_color in TrackingColor:
-        [color_min, color_max] = TRACKED_COLORS_HUE[tracked_color]
-        masked_image=cv2.inRange(hue, color_min, color_max)
-        cv2.imshow("Masked image", masked_image)
-        _, contours, _ = cv2.findContours(
-            masked_image,
-            cv2.RETR_LIST,
-<<<<<<< Updated upstream
-            cv2.CHAIN_APPROX_SIMPLE
-=======
-               cv2.CHAIN_APPROX_SIMPLE
->>>>>>> Stashed changes
-        )
-
-        def filter_contour(contour):
-            perimeter = cv2.arcLength(contour, True)
-            approx = cv2.approxPolyDP(contour, 0.04 * perimeter, True)
-            return len(approx) == 4
-
-        filtered_contours = filter(filter_contour, contours)
+    def process(self, image):
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
         
-        found_colors[tracked_color] = map(cv2.boundingRect, filtered_contours)
+        #plt.imshow(image)
+        #time.sleep(2)
+        cv2.imshow("Original image", image)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            exit()
+        return "Hello, World !"
 
-    (image_width, image_height) = hue.shape
-    for found_color in found_colors:
-        for (x, y, w, h) in found_colors[found_color]:
-            if w > 20 and h > 20 and w < 0.4*image_width and h < 0.4*image_height: #todo, find appropriate thresholds for the area
-                cv2.rectangle(frame, (x,y), (x+w,y+h), (255, 0, 0), 2)
 
+def main():
+    '''Entry point of this file'''
+    processor = ROSImageProcessingWrapper(ZoneDetection, IMAGE_TOPIC)
+    publisher = rospy.Publisher(ZONE_DETECTION_TOPIC, String, queue_size=10, latch=True)
+    processor.loop(lambda processor_output : publisher.publish(processor_output))
 
-    #cv2.imshow('Hue1', tracked_hues)
-    cv2.imshow('Result', frame)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-video_capture.release()
-cv2.destroyAllWindows()
+if __name__ == '__main__':
+    main()
