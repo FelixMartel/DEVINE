691ef580f53d4eb931e498620579fe64afeed8e1
Adam <adam.letourneau1993@gmail.com>
2018-09-25 17:56:18 -0400

    Remove commented methods

--- config/src/DEVINEParameters.py
+++ config/src/DEVINEParameters.py
@@ -4,19 +4,12 @@ import ConfigParser
 import sys
 import os
 
-#paths
-#ROOT_DIR = sys.path[0]
-#FILE_DIR = os.path.join(ROOT_DIR, "Parameters.ini")
+#Path
 CONFIG_FILE_NAME = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'static', 'Parameters.ini')
 
 Config = ConfigParser.RawConfigParser()
 Config.optionxform = str
 Config.read(CONFIG_FILE_NAME)
-
-#ConfigSectionMap("Section")['name'] 
-
-def test():
-    return CONFIG_FILE_NAME
 
 def ConfigSectionMap(section):
     dict = {}
