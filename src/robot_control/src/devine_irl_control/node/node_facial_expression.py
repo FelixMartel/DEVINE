f668d89af8da89bd66a61a086648f72e1fd7c231
Julien Chouinard-Beaupré <zagrivera@gmail.com>
2018-11-25 16:01:52 -0500

    Applied PR review

--- src/robot_control/src/devine_irl_control/node/node_facial_expression.py
+++ src/robot_control/src/devine_irl_control/node/node_facial_expression.py
@@ -72,7 +72,7 @@ class FacialExpression(object):
                 intensity=random.random(),
                 duration=rospy.Duration.from_sec(TALKING_EXPRESSION_DURATION)
                 )
-            rospy.sleep(.1)
+            rospy.sleep(TALKING_EXPRESSION_DURATION)
             duration -= 1
 
         self.showing_emotion = False
